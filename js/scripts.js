5c81d1a69ebb7d50cc147f4571f1e440ce16ee43
Austin Roberts <d.austin.roberts@gmail.com>
2018-04-01 17:35:00 -0700

    This will fix the show/hide css and html code. The correct message now shows when the user submits the form and there is an alert if the user misses a form

--- js/scripts.js
+++ js/scripts.js
@@ -14,21 +14,24 @@ friend = $("#friends").val();
 };
 
 function course(){
+  if (company == 0 || where == 0 || committed == 0 || interest == 0 || friend == 0){
+    alert("Please select an option from each box")
+  }
   if (company === "microsoft" && committed === "yes" && interest === "yes" && friend === "yes"){
-    alert("You should take C#/.NET");
-    $("#Csharp").show();
+    // alert("You should take C#/.NET");
+    $("#cSharp").show();
   }
   if (company === "google" && committed === "yes" && interest === "yes" && friend === "yes"){
-    alert("You should take Java/Android");
-    $("#Java").show();
+    // alert("You should take Java/Android");
+    $("#java").show();
   }
   if (company === "gov" && committed === "yes" && interest === "yes" && friend === "yes"){
-    alert("You should take PHP");
-    $("#PHP").show();
+    // alert("You should take PHP");
+    $("#php").show();
   }
   if (company === "startup" && committed === "yes" && interest === "yes" && friend === "yes"){
-    alert("You should take Ruby/Rails")
-    $("#Ruby").show();
+    // alert("You should take Ruby/Rails")
+    $("#ruby").show();
   // } else{
   //   alert("You should not enroll")
   //   $("#Not").show()
